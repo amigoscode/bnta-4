d3b9e506819947a9445b6913fece190bf20467ff
amigoscode <amigoscode@gmail.com>
2022-01-24 11:00:23 +0000

    exercises from Friday

---
+++ src/com/bnta/exercises/week_one_fri/Exercise5.java
@@ -0,0 +1,15 @@
+package com.bnta.exercises.week_one_fri;
+
+public class Exercise5 {
+    /*
+       Write a program that takes arguments from the program args and loops through args and prints each item in args
+       Compile using: javac and run using: java
+       i.e. javac com.bnta.exercises.week_one_fri.Exercise5.java | java com.bnta.exercises.week_one_fri.Exercise5 foo bar baz
+     */
+    public static void main(String[] args) {
+        // loop through args here
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
