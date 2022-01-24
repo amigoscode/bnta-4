0640674c29cf3657d414979d931ed2afac50ff5a
amigoscode <amigoscode@gmail.com>
2022-01-24 13:26:05 +0000

    exercises

--- src/com/bnta/exercises/week_two_mon/Exercise1.java
+++ src/com/bnta/exercises/week_two_mon/Exercise1.java
@@ -1,7 +1,12 @@
 package com.bnta.exercises.week_two_mon;
 
 public class Exercise1 {
+    /*
+      Write a method that reverses any String and print to console
+      Input should be a string
+      Output should be reversed input
+    */
     public static void main(String[] args) {
-
+        // invoke your function here
     }
 }
