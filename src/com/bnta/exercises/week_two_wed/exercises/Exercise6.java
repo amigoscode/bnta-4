4de2b845490d3947f60aff2abd7e4b4e6604b23c
amigoscode <amigoscode@gmail.com>
2022-01-26 14:06:09 +0000

    exercises

--- src/com/bnta/exercises/week_two_wed/exercises/Exercise6.java
+++ src/com/bnta/exercises/week_two_wed/exercises/Exercise6.java
@@ -3,6 +3,7 @@ package com.bnta.exercises.week_two_wed.exercises;
 public class Exercise6 {
 
     public static void main(String[] args) {
+        // try catch
         // write a program that converts program arguments to and integer
         // if value cannot be converted to int store them somewhere
         // calculate sum for numbers that can be converted to an integer
